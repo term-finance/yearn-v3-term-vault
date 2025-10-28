4f2194cb01432b66a569a40d869e2b45448c04e7
aazhou1 <andrew@termfinance.io>
2025-10-27 20:33:39 -0700

    re-enable optimizer

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -54,7 +54,7 @@ const config: HardhatUserConfig = {
     version: "0.8.18",
     settings: {
       optimizer: {
-        enabled: false,
+        enabled: true,
         runs: 200,
       },
     },
